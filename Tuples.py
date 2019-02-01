3aef90b0d5a3c58c313eff8c578614f3519024d6
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-02 01:49:50 +0300

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#!creating tuples
+
+coordinates = (5, 8) #!tuples can't be changed or modified
+print(coordinates[1])
+print(coordinates[0])
+
+#!N.B the difference between tuples and list is that in list we use square brackets"[]" and in tuples we use parenthesis "()"
+
+#!A list of tuples
+location = [(8, 6), (9, 7), (10, 8), (11, 9)]
+print(location[1])
+
+#!Go ahead and Test it out
